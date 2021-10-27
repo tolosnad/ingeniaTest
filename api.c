e7e0b09a266c8771b0a985d397d15663d8851bd3
tolosnad <93288897+tolosnad@users.noreply.github.com>
2021-10-27 22:31:06 +0200

    Create api.c

---
+++ api.c
@@ -0,0 +1,42 @@
+/* Ingenia API.c */
+
+void RobotTask(uint8_t u8Command) {
+
+}
+
+void RobotStop (void) void TimerInit(void) {
+
+}
+
+Interrupt void TimerISR(void) {
+
+}
+
+uint32_t TimetGetus(void) {
+  uint32_t dummy = 11;
+  return dummy;
+  
+}
+
+void SPIInit(void) {
+
+}
+
+void SPIEepromEnable(void){
+
+}
+
+void SPIEepromDisable(void){
+
+}
+
+void SPISensorEnable(void){
+
+}
+void SPISensorDisable(void){
+
+}
+uint8_t SPIRead(void){
+  uint8_t dummy = 1;
+  return dummy;
+}
