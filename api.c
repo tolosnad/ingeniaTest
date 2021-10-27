ead0ff11383850844340492693385edb17d3bdcc
tolosnad <93288897+tolosnad@users.noreply.github.com>
2021-10-27 23:43:36 +0200

    Handle timer interruption

--- api.c
+++ api.c
@@ -4,12 +4,24 @@ void RobotTask(uint8_t u8Command) {
 
 }
 
-void RobotStop (void) void TimerInit(void) {
+void RobotStop (void) {
+
+}
+
+void TimerInit(void) {
 
 }
 
 Interrupt void TimerISR(void) {
-
+  SPIEepromDisable(); //1us
+  SPISensorEnable(); //1us
+  sensorData = SPIRead(); //5us
+  if(sensorData > 128){
+    RobotStop(); //15us
+  }
+  SPISendorDisable(); //1us
+  SPIEepromEnable(); //1us
+  TimerInit(); 
 }
 
 uint32_t TimetGetus(void) {
@@ -33,10 +45,13 @@ void SPIEepromDisable(void){
 void SPISensorEnable(void){
 
 }
+
 void SPISensorDisable(void){
 
 }
+
 uint8_t SPIRead(void){
   uint8_t dummy = 1;
   return dummy;
 }
+
