6d4f7d0a98ac0f14b15fed28936a7c860d33f01c
tolosnad <93288897+tolosnad@users.noreply.github.com>
2021-10-27 23:42:27 +0200

    use global variable for sensor data lecture

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-/*Ingenia Exercice 3 */
-
 #include api.h
 
 uint8_t sensorData; //global variable, will be updated by the interruption
@@ -11,18 +9,21 @@ int main(void){
   //SPI bus initialization
   SPIInit();
   SPIEepromDisable(); //1us
-  SPISensorEnable(); //1us
+  SPISensorEable(); //1us
   sensorData = SPIRead(); //5us
+  if(sensorData > 128){
+    RobotStop(); //15us
+  }
   SPISensorDisable(); //1us
+  // Enable Eprom
+  SPIEepromEnable(); //1us
   while(true) {
-    // Enable Eprom
-    SPIEpromEnable(); //1us
     //read eprom data
-    epromData = SPIRead();
-    if(sensorData < 128 ) {
-      RobotTask(epromData); //100ms
-    } else {
+    epromData = SPIRead(); //5us
+    if(sensorData > 128){
       RobotStop(); //15us
-    }
+    } else {
+      RobotTask(epromData); //100ms
+    } 
   }   
 }
